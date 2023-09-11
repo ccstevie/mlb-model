9de21d1001cc9531e51f696fbfb3ad03ce29b0da
ccstevie <steven.ly.wang@gmail.com>
2023-09-11 02:09:26 -0400

    data for two ronald games

--- gen_plot.py
+++ gen_plot.py
@@ -6,20 +6,23 @@ Names = []
 Values = []
   
 def main(data=None):
-    plt.figure(figsize=(200,10))
     if not data:
         with open(f'{date.today()}.csv','r') as csvfile:
             lines = csv.reader(csvfile, delimiter=',')
+            count = 10
             for row in lines:
+                if count == 0:
+                    break
                 if len(row) > 0:
                     Names.append(row[0])
                     Values.append(float(row[1]))
+                    count -= 1
         
         plt.scatter(Names, Values, color = 'g', s=10)
         plt.xticks(rotation = 25)
         plt.xlabel('Names')
         plt.ylabel('Values')
-        plt.title('MLB Swamy Scores Today', fontsize = 20)
+        plt.title(f'{date.today()} Swamy Scores', fontsize = 20)
     else:
         x, y = zip(*data)
         plt.plot(x, y)
